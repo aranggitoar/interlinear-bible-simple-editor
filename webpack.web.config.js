118e2bf8adee9438a187e5c733342d241e677055
aranggitoar <aranggi.josef@gmail.com>
2022-01-16 07:56:11 +0700

    GHPage: Added necessary build files and configuration

---
+++ webpack.web.config.js
@@ -0,0 +1,27 @@
+var rules = require('./webpack.rules');
+var plugins = require('./webpack.plugins');
+var path = require('path');
+var TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+module.exports = {
+  entry: './src/web',
+  output: {
+    path: path.resolve(__dirname, 'gh-page/'),
+  },
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    fallback: { 'assert': false,
+                'stream': false },
+  },
+};
+
