6d160a13ead749597e7b3d8cbe62603129fc3291
Aranggi Josef Toar <aranggi.josef@gmail.com>
2022-01-17 11:23:52 +0700

    Logic: added logic to arrange Bible book name

---
+++ src/utilities/arrangeBibleBookName.ts
@@ -0,0 +1,82 @@
+// Array of correct Bible book name order.
+const arrayOfCorrectlyOrderedBibleBookName = [
+  "Genesis",
+  "Exodus",
+  "Leviticus",
+  "Numbers",
+  "Deuteronomy",
+  "Joshua",
+  "Judges",
+  "Ruth",
+  "1 Samuel",
+  "2 Samuel",
+  "1 Kings",
+  "2 Kings",
+  "1 Chronicles",
+  "2 Chronicles",
+  "Ezra",
+  "Nehemiah",
+  "Esther",
+  "Job",
+  "Psalms",
+  "Proverbs",
+  "Ecclesiastes",
+  "Song of Songs",
+  "Isaiah",
+  "Jeremiah",
+  "Lamentations",
+  "Ezekiel",
+  "Daniel",
+  "Hosea",
+  "Joel",
+  "Amos",
+  "Obadiah",
+  "Jonah",
+  "Micah",
+  "Nahum",
+  "Habakkuk",
+  "Zephaniah",
+  "Haggai",
+  "Zechariah",
+  "Malachi",
+  "Matthew",
+  "Mark",
+  "Luke",
+  "John",
+  "Acts",
+  "Romans",
+  "1 Corinthians",
+  "2 Corinthians",
+  "Galatians",
+  "Ephesians",
+  "Philippians",
+  "Colossians",
+  "1 Thessalonians",
+  "2 Thessalonians",
+  "1 Timothy",
+  "2 Timothy",
+  "Titus",
+  "Philemon",
+  "Hebrews",
+  "James",
+  "1 Peter",
+  "2 Peter",
+  "1 John",
+  "2 John",
+  "3 John",
+  "Jude",
+  "Revelation"
+]
+
+// Arrange the array of Bible book names.
+export function arrangeBibleBookName(arrayOfBibleBookNames: Array<string>) {
+  let orderedArrayOfBibleBookNames = [] as Array<string>;
+
+  for (let i = 0; arrayOfCorrectlyOrderedBibleBookName.length > i; i++) {
+    if (arrayOfBibleBookNames.find(name => name.match(arrayOfCorrectlyOrderedBibleBookName[i])) !== undefined) {
+      orderedArrayOfBibleBookNames.push(arrayOfCorrectlyOrderedBibleBookName[i]);
+    }
+  }
+
+  return orderedArrayOfBibleBookNames;
+}
