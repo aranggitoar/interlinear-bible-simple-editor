711d14415fafc4a5d3764a4daa1c3640710be0c5
aranggitoar <aranggi.josef@gmail.com>
2022-01-18 21:43:42 +0700

    Logic: added logic for parsing ByzMT morphology and strongs

--- src/utilities/filterDisplayedMorphologicalData.ts
+++ src/utilities/filterDisplayedMorphologicalData.ts
@@ -10,9 +10,24 @@ const simpleMorphologicalDataReference = {
   V: "Verb",
 }
 
-const primaryMorphologicalData = ['A', 'D', 'N', 'P', 'V'];
-
-const secondaryMorphologicalData = ['C', 'R', 'T'];
+// Unique morphological codes of the Byzantine Majority Text
+// References:
+// https://github.com/byztxt/robinson-documentation/blob/master/doc/DECLINE.COD
+// https://github.com/byztxt/robinson-documentation/blob/master/doc/PARSING.COD
+const simpleByzMTMorphologicalDataReference = {
+  A: "Adjective",
+  N: "Noun",
+  P: "Pronoun",
+  T: "Definite Article",
+  V: "Verb",
+  ADV: "Adverb",
+  ARAM: "Aramaic Transliteration",
+  CONJ: "Conjunction",
+  COND: "Conditional Particle",
+  HEB: "Hebrew Transliteration",
+  INJ: "Interjection",
+  PREP: "Preposition",
+}
 
 // Parse the inserted morphological data.
 function parser(morphologicalData: string) {
@@ -46,13 +61,90 @@ function parser(morphologicalData: string) {
   return parsedMorphologicalData;
 }
 
+function byzMTParser(morphologicalData: string) {
+  let parsedMorphologicalData = morphologicalData;
+    switch(parsedMorphologicalData) {
+      case 'ADV':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['ADV'];
+        break;
+      case 'ARAM':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['ARAM'];
+        break;
+      case 'CONJ':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['CONJ'];
+        break;
+      case 'COND':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['COND'];
+        break;
+      case 'HEB':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['HEB'];
+        break;
+      case 'INJ':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['INJ'];
+        break;
+      case 'PREP':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['PREP'];
+        break;
+      case 'C': // Reciprocal pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'D': // Demonstrative pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'F': // Reflexive pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'I': // Interrogative pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'K': // Correlative pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'N':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['N'];
+        break;
+      case 'P':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'Q': // Correlative or interrogative pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'R': // Relative pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+      case 'S': // Possesive adjective
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['A'];
+        break;
+      case 'T':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['T'];
+        break;
+      case 'V':
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['V'];
+        break;
+      case 'X': // Indefinite pronoun
+        parsedMorphologicalData = simpleByzMTMorphologicalDataReference['P'];
+        break;
+    }
+  return parsedMorphologicalData;
+}
+
 // Filter the displayed morphological data.
 export function filterDisplayedMorphologicalData(morphologicalData: string) {
+  let parsedMorphologicalData: string;
+
+  // If file is not OSHB.
+  if (morphologicalData[0] !== "H") {
+    if (/[-]/.test(morphologicalData)) {
+      parsedMorphologicalData = byzMTParser(morphologicalData[0])
+    } else {
+      parsedMorphologicalData = byzMTParser(morphologicalData)
+    }
+    return parsedMorphologicalData;
+  }
+
   // Get the main morphological data codes into an array.
   let mainMorphologicalDataPoints = morphologicalData.match(/[A-Z]/g);
   mainMorphologicalDataPoints.shift();
-
-  let parsedMorphologicalData: string;
 
   console.log(morphologicalData);
 
--- src/utilities/filterDisplayedStrongsData.ts
+++ src/utilities/filterDisplayedStrongsData.ts
@@ -1,6 +1,16 @@
 // Filter the displayed strongs data.
 export function filterDisplayedStrongsData(strongsData: string) {
   let filteredStrongsData = strongsData as string;
+
+  if (filteredStrongsData[0] === "G") {
+    if (filteredStrongsData.indexOf('&') > -1) {
+      filteredStrongsData = filteredStrongsData.replace('&', ' & ');
+    }
+    filteredStrongsData = filteredStrongsData.replace(/[G]/g,'');
+
+    return filteredStrongsData;
+  }
+
   if (/\//.test(strongsData)) {
     let temp = strongsData.split('/');
     if (temp.length === 3) {
