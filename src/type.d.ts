057679de29c67a3ddb60a295bff676d0b6c9a67e
Aranggi Josef Toar <aranggi.josef@gmail.com>
2022-01-25 12:19:14 +0700

    Logic: added more necessary global types

--- src/type.d.ts
+++ src/type.d.ts
@@ -5,8 +5,26 @@ interface ILoadedBible {
   chosenBibleBookDetails: Array<string>;
 }
 
+interface ILoadedVerse {
+  arrayOfTargetWords: Array<string>;
+  arrayOfOriginalWords: Array<string>;
+  arrayOfStrongs: Array<string>;
+  arrayOfMorphologies: Array<string>;
+  chosenBibleBookDetails: Array<string>;
+}
+
 type LoadedBibleContextType = {
   loadedBibleObject: ILoadedBible;
   downloadBible: (bibleObject: Object) => void;
   updateUploadedBible: (newlyLoadedBibleObject: ILoadedBible) => void;
 } 
+
+type Props = {
+  loadedBibleObject: ILoadedBible;
+  updateUploadedBible?: (newlyLoadedBibleObject: ILoadedBible) => void;
+}
+
+type VerseDataProps = {
+  loadedBibleVerse: ILoadedVerse;
+  verseIndex: number;
+}
