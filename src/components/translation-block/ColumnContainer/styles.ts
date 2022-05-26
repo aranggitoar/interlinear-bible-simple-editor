0c9734676826107480bc3aea89e24af92617bd06
aranggitoar <aranggi.josef@gmail.com>
2022-05-26 07:08:37 +0700

    Idem

--- src/components/translation-block/ColumnContainer/styles.ts
+++ src/components/translation-block/ColumnContainer/styles.ts
@@ -5,3 +5,12 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const ColumnContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 0;
+  max-width: 75%;
+`;
